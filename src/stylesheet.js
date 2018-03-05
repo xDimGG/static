e57e41ee591b525b6d7d8ddf82c4a29ca87a3aa4
Dim <skyuphigh01@gmail.com>
2018-03-05 17:12:08 -0500

    Create stylesheet.js

---
+++ src/stylesheet.js
@@ -0,0 +1,71 @@
+
+.wbiscroller 
+{
+    width: 100%;
+    font-family: Arial, Helvetica, sans-serif;
+    font-size: 10pt;
+    border-bottom: solid 1px #DBDBDB;
+    background-image: url('http://widgets.freestockcharts.com/WidgetServer/images/BGBlackLine.png');
+	height:16px;
+	width:100%;
+	display:block;
+	position:relative;
+	overflow:hidden;
+	border:#CCCCCC 1px solid;
+	line-height:normal;
+}
+.wbiscroller .changeup{
+    background-image: url('http://widgets.freestockcharts.com/WidgetServer/images/greenGridBG.png');
+    text-align:right;
+    padding-left:3px;
+    padding-right:3px;
+
+}
+.wbiscroller .changedown{
+    background-image: url('http://widgets.freestockcharts.com/WidgetServer/images/redGridBG.png');
+    text-align:right;
+    padding-left:3px;
+    padding-right:3px;
+}
+.wbiscroller .quotedown{
+    text-align:center;
+}
+.wbiscroller .quoteup{
+    text-align:center;
+}
+.wbiscroller .dirup{
+    margin-right:4px;
+}
+.wbiscroller .dirdown{
+    margin-right:4px;
+}
+
+.wbiscroller .sym
+{
+    padding-left:3px;
+    text-align:left;
+    text-transform: uppercase;
+    color:white;
+}
+	div.scrollingtext{
+		position:absolute;
+		white-space:nowrap;
+	}
+
+.valueChanged
+ {
+     font-weight:bold;
+ }
+.wbiscroller a
+{
+     text-decoration:none;
+ }
+.wbiscroller a:hover 
+{
+     text-decoration:underline;
+}
+.WLSymbolItem 
+{
+    margin-right:5px;
+    margin-left:10px;
+}
